702773a85de6099b00f393fc2c2fffce831a2fd3
lzzy12 <jhashivam2020@gmail.com>
2022-09-11 20:04:02 +0530

    Added Q9 and 11

--- assignment-3/11.c
+++ assignment-3/11.c
@@ -1,13 +1,41 @@
 #include "COMMON.h"
 
-SingleLL* solve(SingleLL *head, SingleLL *curr, SingleLL *last){
-    if (last->next){
-        solve(head, curr, last->next);
-    } else{
-        return 
+SingleLL* reverseList(SingleLL *head){
+    SingleLL *ptr = head, *prev = NULL;
+    while (ptr) {
+        SingleLL *next = head;
+        ptr->next = prev;
+        prev = ptr;
+        ptr = next;
     }
-    return head;
+    return prev;
+}
+void solve(SingleLL *head){
+    SingleLL *mid = head, *fast = head;
+    while (fast) {
+        mid = mid->next;
+        fast = fast->next;
+        if (fast)
+            fast = fast->next;
+    }
+    mid = reverseList(mid); // reversed mid
+    fast = head;
+    while (mid && fast) {
+            SingleLL *next = fast->next;
+            fast->next = mid;
+            mid = mid->next;
+            fast = fast->next;
+            fast->next = next;
+            fast = fast->next;
+        }
+        fast->next = NULL;
 }
 int main(){
-
+    SingleLL *head;
+    createLL(&head, 10);
+    printf("Before reorder: ");
+    displayLL(head);
+    solve(head);
+    printf("After reorder: ");
+    displayLL(head);
 }
--- assignment-3/9.c
+++ assignment-3/9.c
@@ -1,5 +1,34 @@
 #include "COMMON.h"
+#include <stdio.h>
 
+SingleLL* rotateRight(SingleLL* head, int k) {
+        if (head == NULL || head->next == NULL)
+            return head;
+        SingleLL *ptr = head;
+        int size = 0;
+        while(ptr){
+            size++;
+            ptr = ptr->next;
+        }
+        k = k % size;
+        while(k--){
+            SingleLL *prev = NULL, *ptr = head;
+            while(ptr->next){
+                prev = ptr;
+                ptr = ptr->next;
+            }
+            ptr->next = head;
+            prev->next = NULL;
+            head = ptr;
+        }
+        return head;
+    }
 int main(){
-    
+    SingleLL *head;
+    createLL(&head, 10);
+    printf("Before rotation: ");
+    displayLL(head);
+    head = rotateRight(head, 7);
+    printf("After rotation: ");
+    displayLL(head);
 }
